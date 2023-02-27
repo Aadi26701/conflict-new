4c70f5b2e4e3ca683ef3d3271f4525e3c3952a19
Aadi26701 <124770939+Aadi26701@users.noreply.github.com>
2023-02-27 15:55:32 +0530

    Update Homepage.tsx

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -20,9 +20,9 @@ const Homepage: FunctionComponent = () => {
           <div className={styles.title}>
             <div
               className={styles.heroTagline}
-            >{`Let’s explore & travel the locofy world`}</div>
+            >{`Let’s explore & travel the github world`}</div>
             <div className={styles.heroSubtext}>
-              Find the best locofy destinations and the most popular stays!
+              Find the best github destinations and the most popular stays!
             </div>
           </div>
           <div className={styles.searchForm}>
